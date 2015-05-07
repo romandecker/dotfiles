9103c4b26828e99725c96a9111c15bf5f48acb53
Roman Decker <roman.decker@card-emotion.com>
2015-05-07 10:40:18 +0200

    Added vimdiff alias on mac

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,10 @@
 alias 'new-project'='tmuxifier ns'
 alias 'edit-project'='tmuxifier es'
 alias project='tmuxifier s'
+
+if [[ `uname` == 'Darwin' ]]
+then
+    # make sure to use macvim here, as it plays better with OSX
+    alias vim='mvim -v'
+    alias vimdiff='mvimdiff -v'
+fi
