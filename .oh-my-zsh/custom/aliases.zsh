e3e112a23fa85f4acbafc4148aef2c0c9472f3f9
Roman Decker <roman.decker@card-emotion.com>
2015-05-28 09:57:10 +0200

    Added more aliases and support for local zsh-config

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,5 +1,16 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+#
 alias 'new-project'='tmuxifier ns'
 alias 'edit-project'='tmuxifier es'
+alias 'list-projects'='tmuxifier ls'
+alias 'grep-projects'='tmuxifier ls | grep -i'
 alias project='tmuxifier s'
 
 if [[ `uname` == 'Darwin' ]]
--- .zshrc
+++ .zshrc
@@ -70,11 +70,8 @@ source $ZSH/oh-my-zsh.sh
 # ssh
 # export SSH_KEY_PATH="~/.ssh/dsa_id"
 
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
+if [ -f ~/.zshrc.local ]; then
+    # if a local config file exists, source it
+    source ~/.zshrc.local
+fi
+
