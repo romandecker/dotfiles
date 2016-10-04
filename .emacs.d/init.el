5a77be496699ef826bc7c8d89ef6c6f5e56fa358
Roman Decker <roman.decker@gmail.com>
2016-10-04 11:18:54 +0200

    Disabled errors and warnings of js2-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -158,7 +158,12 @@
 
 (use-package js2-mode
   :ensure t
-  :config)
+  :config
+  (js2-mode-hide-warnings-and-errors)          ; do not show errors (use flycheck for that)
+  (setq
+   js2-skip-preprocessor-directives nil)       ; allow shebangs in js-files (for node)
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-to-list 'interpreter-mode-alist '("node" . js2-mode)))
 
 (use-package rainbow-delimiters
   :ensure t
