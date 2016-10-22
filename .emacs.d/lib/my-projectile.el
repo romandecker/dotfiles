dae81ec190cb22aa78055f788cd6f13f70139558
Roman Decker <roman.decker@gmail.com>
2016-10-22 11:11:44 +0200

    Made `SPC p p` a bit smarter

--- .emacs.d/lib/my-projectile.el
+++ .emacs.d/lib/my-projectile.el
@@ -12,6 +12,7 @@
     :ensure t
     :config
     (setq wg-morph-on nil)
+    (add-hook 'wg-switch-hook #'my/wg-switch-hook)
     (workgroups-mode 1)
     (when (file-exists-p my/workgroups-file)
       (wg-load my/workgroups-file))
@@ -21,6 +22,13 @@
 (defun my/save-workgroups ()
   (when (wg-list t)
     (wg-update-all-workgroups-and-save)))
+
+(defun my/wg-switch-hook ()
+  (when (string= "*scratch*" (buffer-name (current-buffer)))
+    (helm
+     :sources '(helm-source-projectile-projects)
+     :input (wg-name (wg-current-workgroup)))
+    (helm-projectile-switch-project)))
 
 
 (provide 'my-projectile)
