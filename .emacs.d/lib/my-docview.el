cc679475f9b46b2438734fd6117f047e5f6ab2ae
Roman Decker <roman.decker@gmail.com>
2016-11-09 15:44:38 +0100

    Added some LaTeX capabilities

--- .emacs.d/lib/my-docview.el
+++ .emacs.d/lib/my-docview.el
@@ -2,18 +2,25 @@
 ;;; Commentary:
 ;;; Code:
 
+(setq doc-view-continuous t)
+
+(add-to-list 'evil-emacs-state-modes 'doc-view-mode)
+(add-to-list 'evil-leader/no-prefix-mode-rx "doc-view-mode")
+
 (defun my/evil-doc-view-hook ()
-  (turn-off-evil-mode)
+  ;;(turn-off-evil-mode)
   (delim-pad-mode -1)
   (define-key doc-view-mode-map (kbd "j")   'doc-view-next-line-or-next-page)
   (define-key doc-view-mode-map (kbd "k")   'doc-view-previous-line-or-previous-page)
   (define-key doc-view-mode-map (kbd "C-d") 'doc-view-scroll-up-or-next-page)
   (define-key doc-view-mode-map (kbd "C-u") 'doc-view-scroll-down-or-previous-page)
+  (define-key doc-view-mode-map (kbd "d")   'doc-view-scroll-up-or-next-page)
+  (define-key doc-view-mode-map (kbd "u")   'doc-view-scroll-down-or-previous-page)
   (define-key doc-view-mode-map (kbd "h")   'image-backward-hscroll)
   (define-key doc-view-mode-map (kbd "l")   'image-forward-hscroll))
 
-(add-hook 'doc-view-mode-hook #'my/evil-doc-view-hook)
 
+(add-hook 'doc-view-mode-hook #'my/evil-doc-view-hook)
 
 (provide 'my-docview)
 ;;; my-docview.el ends here
--- .emacs.d/lib/my-latex.el
+++ .emacs.d/lib/my-latex.el
@@ -2,27 +2,48 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package latex-preview-pane
+  :ensure t
+  :config
+  (latex-preview-pane-enable))
+
 (require 'langtool)
 
-;; (setq langtool-language-tool-jar
-;;       "~/bin/LanguageTool-3.5/languagetool-commandline.jar")
+(setq langtool-language-tool-jar
+      "~/bin/LanguageTool-3.5/languagetool-commandline.jar")
 
-;; (setq langtool-default-language "en-US")
+(setq langtool-default-language "en-US")
 
-;; (defun langtool-autoshow-detail-popup (overlays)
-;;   (when (require 'popup nil t)
-;;     ;; Do not interrupt current popup
-;;     (unless (or popup-instances
-;;                 ;; suppress popup after type `C-g` .
-;;                 (memq last-command '(keyboard-quit)))
-;;       (let ((msg (langtool-details-error-message overlays)))
-;;         (popup-tip msg)))))
+(defun langtool-autoshow-detail-popup (overlays)
+  (when (require 'popup nil t)
+    ;; Do not interrupt current popup
+    (unless (or popup-instances
+                ;; suppress popup after type `C-g` .
+                (memq last-command '(keyboard-quit)))
+      (let ((msg (langtool-details-error-message overlays)))
+        (popup-tip msg)))))
 
-;; (setq langtool-autoshow-message-function
-;;       'langtool-autoshow-detail-popup)
+(setq langtool-autoshow-message-function
+      'langtool-autoshow-detail-popup)
 
-;; (evil-leader/set-key-for-mode 'tex-mode
-;;     "SPC t s"   'langtool-check)
+(evil-leader/set-key-for-mode 'latex-mode
+  "SPC s" 'langtool-check
+  "SPC S" 'langtool-check-done)
+
+(add-hook 'latex-mode-hook '(flyspell-mode t))
+
+(evil-leader/set-key
+  "t s" 'flyspell-prog-mode)
+
+(evil-define-key 'normal flyspell-mode-map
+  (kbd "] s") 'flyspell-goto-next-error)
+
+(use-package flyspell-popup
+  :ensure t
+  :config
+  (add-hook 'flyspell-mode-hook #'flyspell-popup-auto-correct-mode)
+  (evil-define-key 'normal flyspell-mode-map
+    (kbd "g =") 'flyspell-popup-correct))
 
 (provide 'my-latex)
 ;;; my-latex.el ends here
