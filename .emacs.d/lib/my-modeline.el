3d687ede0c22312e1e96e8c8e96a4c284b597b9f
Roman Decker <rd@innovation.rocks>
2016-12-15 16:50:57 +0100

    :construction: Started custom modeline implementation

--- .emacs.d/lib/my-modeline.el
+++ .emacs.d/lib/my-modeline.el
@@ -7,5 +7,48 @@
   (setq sml/theme 'automatic)
   (sml/setup))
 
+
+
+
+;; TODO some WIP-code for custom modeline below
+
+;; use force-mode-line-update to update modeline!
+(require 's)
+(defcustom
+  my/path-transformers
+  '(my/shorten-home my/shorten-projects my/shorten-dotfiles)
+  "List of replacers that will be applied in order to (buffer-file-name) before displaying it in the mode-line")
+
+
+(defun my/shorten-home (full-path)
+  "Transform /Users/roman/foo or /home/roman/foo to ~/foo."
+  (let ((home (expand-file-name "~")))
+    (if (s-starts-with? home full-path)
+        (concat "~" (substring full-path (length home)))
+      full-path)))
+
+(defun my/shorten-projects (path)
+  (replace-regexp-in-string "~/projects/" ":P:/" path))
+
+(defun my/shorten-dotfiles (path)
+  (replace-regexp-in-string "~/.dotfiles/" ":.:/" path))
+
+(defun my/replacer-pipeline ()
+  (let ((path (buffer-file-name)))
+    (dolist (replacer my/path-transformers)
+      (if (functionp replacer)
+          (progn
+            (message "applying %s" replacer)
+            (setq path (apply replacer `(,path))))))
+    path))
+
+;; (setq mode-line-format
+;;       (list
+;;        " "
+;;        '(:eval (propertize (my/replacer-pipeline) 'face 'font-lock-keyword-face))))
+
+(force-mode-line-update)
+
+
 (provide 'my-modeline)
 ;;; my-modeline.el ends here
