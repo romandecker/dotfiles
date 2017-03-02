e21fcc6993f908a4b1faae69e7c050c853fdecf1
Roman Decker <rd@innovation.rocks>
2017-03-02 14:48:14 +0100

    :bug: Fixed `ds` deleting stuff between point and surround start

--- .emacs.d/lib/my-evil.el
+++ .emacs.d/lib/my-evil.el
@@ -67,6 +67,10 @@
       "p" 'my/formatted-paste-after
       "P" 'my/formatted-paste-before))
 
+  (general-define-key :keymaps 'normal "d"
+    (general-key-dispatch 'evil-delete
+      "s" 'evil-surround-delete))
+
   (setq evil-insert-state-cursor '((bar . 3) "red")
         evil-normal-state-cursor '(box "black"))
   (use-package evil-surround
