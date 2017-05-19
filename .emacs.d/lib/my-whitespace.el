0f957f324ea0e20e21e166685aaf524c099877c6
Roman Decker <roman.decker@gmail.com>
2017-05-19 08:12:41 +0200

    :sparkles: Add whitespace cleanup mode

---
+++ .emacs.d/lib/my-whitespace.el
@@ -0,0 +1,12 @@
+;;; package --- My custom whitespace config
+;;; Commentary:
+;;; Code:
+
+(use-package ws-butler
+  :ensure t
+  :config
+  (global-whitespace-cleanup-mode))
+
+
+(provide 'my-whitespace)
+;;; my-whitespace.el ends here
