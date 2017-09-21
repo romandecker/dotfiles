1b8850361d4c2d620cc48c993b2f561097b818c9
Roman Decker <roman.decker@gmail.com>
2017-09-21 17:11:46 +0200

    :wrench: More i3 config

--- installer/005-fonts.sh
+++ installer/005-fonts.sh
@@ -8,3 +8,20 @@ source $SCRIPT_DIR/utils.sh
 pushd $SCRIPT_DIR/../submodules/powerline-fonts
 ./install.sh
 popd
+
+FONT_WORKDIR=/tmp/fonts-$(date "+%s")
+mkdir -p $FONT_WORKDIR
+
+pushd $FONT_WORKDIR
+curl http://fontawesome.io/assets/font-awesome-4.7.0.zip > fontawesome.zip
+unzip fontawesome.zip
+
+# cd into the extracted folder (cannot be sure what the name is,
+# because it contains version number)
+pushd */
+
+mv ./fonts/fontawesome-webfont.ttf ~/.fonts/
+
+popd
+popd
+
