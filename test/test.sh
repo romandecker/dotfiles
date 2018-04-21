2ad381843fe0e1ead782310eaa40861a87700b98
Roman Decker <roman.decker@gmail.com>
2018-04-21 12:20:26 +0200

    :bug: Fixed test script to be able to run when clean

--- test/test.sh
+++ test/test.sh
@@ -6,17 +6,24 @@ REPO_DIR=$SCRIPT_DIR/..
 WORKSPACE_DIR=$SCRIPT_DIR/workspace
 TESTING_BRANCH=test-installer
 
-git stash --include-untracked > /dev/null
+IS_CLEAN=$(git status --porcelain)
+
+if [ -n "$IS_CLEAN" ]; then
+  git stash --include-untracked > /dev/null
+fi
+
 
 git rev-parse --verify $TESTING_BRANCH && git branch -D $TESTING_BRANCH > /dev/null
 
 echo "Ensuring branch $TESTING_BRANCH to hold the current repo state"
 git checkout -b $TESTING_BRANCH
 
-git stash apply > /dev/null
+if [ -n "$IS_CLEAN" ]; then
+  git stash apply > /dev/null
+fi
 
 git add $REPO_DIR > /dev/null
-git commit -m "Commit for test" > /dev/null
+git commit --allow-empty -m "Commit for test" > /dev/null
 git checkout -
 git stash pop > /dev/null
 
