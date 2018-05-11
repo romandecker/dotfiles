f5fd2093feee90e371c43d8a5ed338b0586e2ee3
Roman Decker <Roman.Decker@zooplus.com>
2018-05-11 14:34:04 +0200

    :heavy_plus_sign: Add empty vscode config

---
+++ installer/012-vscode.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $SCRIPT_DIR/utils.sh
+
+VSCODE_USER_CONFIG_DIR="Library/Application Support/Code/User"
+
+if [ $os == $LINUX ]; then
+  VSCODE_USER_CONFIG_DIR=".config/Code/User"
+fi
+
+link_dotfile vscode "$VSCODE_USER_CONFIG_DIR"
