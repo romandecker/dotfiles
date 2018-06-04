f5bb2e0a9fcbdaac25b71e574c78281417b89914
Roman Decker <Roman.Decker@zooplus.com>
2018-06-04 16:47:53 +0200

    :sparkles: Add auto-minor-mode-alist

--- .emacs.d/lib/my-utils.el
+++ .emacs.d/lib/my-utils.el
@@ -437,5 +437,30 @@ into the current buffer."
     (message "Yanked \"%s\"" file-name)))
                      
 
-(provide 'my-utils))
+(defvar my/auto-minor-mode-alist ()
+  "Alist of filename patterns vs correpsonding minor mode functions, see `auto-mode-alist'
+All elements of this alist are checked, meaning you can enable multiple minor modes for the same regexp.")
+
+(defun my/enable-minor-mode-based-on-extension ()
+  "check file name against my/auto-minor-mode-alist to enable minor modes
+the checking happens for all pairs in my/auto-minor-mode-alist"
+  (when buffer-file-name
+    (let ((name buffer-file-name)
+          (remote-id (file-remote-p buffer-file-name))
+          (alist my/auto-minor-mode-alist))
+      ;; Remove backup-suffixes from file name.
+      (setq name (file-name-sans-versions name))
+      ;; Remove remote file name identification.
+      (when (and (stringp remote-id)
+                 (string-match-p (regexp-quote remote-id) name))
+        (setq name (substring name (match-end 0))))
+      (while (and alist (caar alist) (cdar alist))
+        (if (string-match (caar alist) name)
+            (funcall (cdar alist) 1))
+        (setq alist (cdr alist))))))
+
+(add-hook 'find-file-hook 'my/enable-minor-mode-based-on-extension)
+
+
+(provide 'my-utils)
 ;;; my-utils.el ends here
