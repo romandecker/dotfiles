c397135a2933585ce00f0a6d5c07092eafae736c
Roman Decker <Roman.Decker@zooplus.com>
2018-07-12 16:05:33 +0200

    :sparkles: First version of JIRA package

--- .emacs.d/lib/my-jira.el
+++ .emacs.d/lib/my-jira.el
@@ -4,14 +4,20 @@
 ;;; Code:
 
 
-(defvar jira/url nil "JIRA url to use, should include protocol.")
+(defvar jira/url nil "JIRA url to use, should include protocol and not end in a slash.")
 (defvar jira/board-id nil "ID of the JIRA-board you want to work with.")
 (defvar jira/username nil "Username to use when logging in to JIRA.")
 (defvar jira/password nil "Password to use when logging in to JIRA.")
+(defvar jira/stash-url nil "Stash url to use (for bitbucket API, should include protocol and not end in a slash).")
+(defvar jira/project nil "The JIRA project to use for bitbucket API requests.")
+(defvar jira/repo nil "The BitBucket repo to use for bitbucket API requests.")
 
 (setq jira/url "https://tracker.zooplus.de"
       jira/board-id 763
-      jira/username "romande")
+      jira/username "romande"
+      jira/stash-url "https://src.private.zooplus.net"
+      jira/project "prs"
+      jira/repo "prs")
 
 
 (defun jira/build-basic-auth-token ()
@@ -27,17 +33,22 @@
   (when (not jira/password)
     (jira/read-password)))
 
-
 (defun jira/read-password ()
   "Read a new value for `jira/password'."
   (setq jira/password (read-passwd (format "JIRA-Password for %s: " jira/username)))
   nil)
 
+(defun jira/logout ()
+  "Unset `jira/password'."
+  (interactive)
+  (setq jira/password nil)
+  (message "Cleared JIRA password"))
+
 (defun jira/request (&rest args)
   "Call `request' with the supplied `ARGS', but ensure that a password is set and credentials are supplied."
   (jira/ensure-password)
   (apply 'request (append args
-                          '(:headers `(("Authorization" . ,(jira/build-auth-header)))))))
+                          `(:headers (("Authorization" . ,(jira/build-auth-header)))))))
 
 (defun jira/fetch-issues (callback)
   "Fetch all open issues for the configured board and call `CALLBACK' with the resulting list."
@@ -51,6 +62,14 @@
              (lambda (&key data &allow-other-keys)
                (funcall callback (alist-get 'issues data))))))
 
+(defun jira/fetch-pull-requests (callback)
+  "Fetch all open pull requests for the configured project and repo and call `CALLBACK' with the resulting list."
+  (jira/request
+   (format "%s/rest/api/1.0/projects/%s/repos/%s/pull-requests" jira/stash-url jira/project jira/repo)
+   :parser 'json-read
+   :success (function* (lambda (&key data &allow-other-keys)
+                         (funcall callback (alist-get 'values data))))))
+
 (defun jira/get-issue-details (issue-id callback)
   "Fetch the details for a single issue by its `ISSUE-ID' (=purely numeric, not its key), and call `CALLBACK' with the resulting list of issues."
   (request
@@ -58,7 +77,6 @@
    :params `(("issueId" . ,issue-id)
              ("applicationType" . "stash")
              ("dataType" . "pullrequest"))
-   :parser 'json-read
    :success (function* (lambda (&key data &allow-other-keys)
                          (funcall callback data)))))
 
@@ -73,6 +91,14 @@
    issues))
 
 
+(defun jira/build-candidate-list-from-pull-requests (pull-requests)
+  "Take `PULL-REQUESTS' as returned by jira/fetch-pull-requests and build a suitable candidate list for helm with it."
+  (mapcar
+   (lambda (pr)
+     (let* ((title (alist-get 'title pr)))
+       `(,(format "%s" title) . ,pr)))
+   pull-requests))
+
 (defun jira/browse-issue ()
   "Fetch a list of issues from JIRA and prompt for selection of one to open in the browser."
   (interactive)
@@ -81,13 +107,49 @@
      (let* ((helm-source
              (helm-build-sync-source "jira-issues-source"
                :candidates (jira/build-candidate-list-from-issues issues)
-               :action (helm-make-actions "Open in browser" #'jira/helm-action-open-in-browser))))
+               :action (helm-make-actions "Open in browser" #'jira/helm-action-open-issue-in-browser))))
        (helm :sources helm-source)))))
 
-(defun jira/helm-action-open-in-browser (issue)
+(defun jira/browse-pull-request ()
+  "Fetch a list of pull-requests from Bitbucket and prompt for selection of one to open in the browser."
+  (interactive)
+  (jira/fetch-pull-requests
+   (lambda (pull-requests)
+     (let* ((helm-source
+             (helm-build-sync-source "jira-pull-requests-source"
+               :candidates (jira/build-candidate-list-from-pull-requests pull-requests)
+               :action (helm-make-actions "Open in browser" #'jira/helm-action-open-pull-request-in-browser))))
+       (helm :sources helm-source)))))
+
+(defun jira/magit-checkout-pull-request ()
+  "Fetch a list of pull-requests from Bitbucket and prompt for selection of one to open in the browser."
+  (interactive)
+  (jira/fetch-pull-requests
+   (lambda (pull-requests)
+     (let* ((helm-source
+             (helm-build-sync-source "jira-pull-requests-source"
+               :candidates (jira/build-candidate-list-from-pull-requests pull-requests)
+               :action (helm-make-actions "Check-out" #'jira/helm-action-checkout-pull-request))))
+       (helm :sources helm-source)))))
+
+
+(defun jira/helm-action-open-issue-in-browser (issue)
   "Open the given `ISSUE' in the browser."
   (let ((key (alist-get 'key issue)))
     (browse-url (format "%s/browse/%s" jira/url key))))
+
+(defun jira/helm-action-open-pull-request-in-browser (pull-request)
+  "Open the given `PULL-REQUEST' in the browser."
+  (let* ((links (alist-get 'links pull-request))
+         (self (elt (alist-get 'self links) 0))
+         (href (alist-get 'href self)))
+    (browse-url href)))
+
+(defun jira/helm-action-checkout-pull-request (pull-request)
+  "Check-out the given `PULL-REQUEST' using magit (branch has to already exist currently)."
+  (let* ((from-ref (alist-get 'fromRef pull-request))
+         (display-id (alist-get 'displayId from-ref)))
+    (magit-checkout display-id)))
 
 (provide 'my-jira)
 ;;; my-jira.el ends here
