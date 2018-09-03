38b16ac76d97fce0e17cc4398a0bea189b41a64d
Roman Decker <Roman.Decker@zooplus.com>
2018-09-03 09:23:22 +0200

    :sparkles: Fix typescript config

--- .emacs.d/lib/my-typescript.el
+++ .emacs.d/lib/my-typescript.el
@@ -4,11 +4,36 @@
 
 (use-package tide
   :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode))
+  :general
+  (:states 'normal
+   :keymaps 'typescript-mode-map
+   "K" 'tide-documentation-at-point)
+  (:prefix my/local-leader
+   :states 'normal
+   :keymaps 'typescript-mode-map
+   "*" 'tide-references
+   "e" 'tide-project-errors
+   "r" 'tide-rename-symbol
+   "R" 'tide-rename-file
+   "f" 'tide-format
+   "c" 'tide-jsdoc-template
+   "?" 'tide-verify-setup
+   "i" 'tide-organize-imports)
+
   :config
-  (defun my/tide-setup ()
-    (interactive)
-    (tide-setup))
-  (add-hook 'typescript-mode-hook #'my/setup-tide-mode))
+
+  ;; make sure "q" works in documentation buffer
+  (defun my/advice-tide-doc-buffer (&rest args)
+    (with-current-buffer "*tide-documentation*"
+      (evil-local-set-key 'normal "q" #'quit-window)))
+  (advice-add 'tide-doc-buffer :after #'my/advice-tide-doc-buffer)
+
+  (setq tide-completion-detailed t)
+
+  )
 
 
 (provide 'my-typescript)
