ee5f66f36afd92906dff0b7d053fd6db3d08e15a
Roman Decker <Roman.Decker@zooplus.com>
2019-01-04 14:58:51 +0100

    Add skeleton for multiple targets

--- hygen-templates/project/typescript-library/dotdir-locals.el
+++ hygen-templates/project/typescript-library/dotdir-locals.el
@@ -1,13 +1,20 @@
 ---
 to: <%= cwd %>/<%= projectName %>/.dir-locals.el %>
 ---
-<% const rootDir = `${cwd}/${projectName}`; %>
+<%
+const rootDir = `${cwd}/${projectName}`;
+const tabWidth = locals.tabWidth || 2;
+%>
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 ((nil
      (fill-column . 100)
      (create-lockfiles nil)
      (prettier-args "--config" "<%= rootDir %>/.prettierrc.json"))
+     (evil-shift-width . <%= tabWidth %>)
+     (js-indent-level . <%= tabWidth %>)
+     (js2-basic-offset . <%= tabWidth %>)
+     (tab-width . <%= tabWidth %>)
     (typescript-mode
      (flycheck-typescript-tslint-executable . "<%= rootDir %>/node_modules/.bin/tslint")
      (flycheck-tslint-args . ("--project" "<%= rootDir %>/tsconfig.json"))))
--- hygen-templates/project/typescript-library/index.js
+++ hygen-templates/project/typescript-library/index.js
@@ -1,11 +1,29 @@
 module.exports = {
-  prompt: ({ inquirer, args }) => {
+  prompt: async ({ inquirer, args }) => {
+    const answers = {};
     if (!args.projectName) {
-      return inquirer.prompt({
-        type: 'input',
-        name: 'projectName',
-        message: 'Project name:'
-      });
+      Object.assign(
+        answers,
+        await inquirer.prompt({
+          type: 'input',
+          name: 'projectName',
+          message: 'Project name:'
+        })
+      );
     }
+
+    // Object.assign(
+    //   answers,
+    //   await inquirer.prompt({
+    //     type: 'checkbox',
+    //     name: 'targets',
+    //     message: 'Target platform(s):',
+    //     choices: ['Node', 'Browser'],
+    //     default: ['Node'],
+    //     validate: answers => answers.length >= 1 || 'Must select at least one'
+    //   })
+    // );
+
+    return answers;
   }
 };
