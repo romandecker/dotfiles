51d4efe832158dfa53441de25e59090f815df158
Roman Decker <Roman.Decker@zooplus.com>
2019-03-28 14:13:54 +0100

    Improve docker-cleanup script

--- zsh-lib/docker-cleanup.sh
+++ zsh-lib/docker-cleanup.sh
@@ -1,3 +1,47 @@
 docker-cleanup() {
-  docker rmi $(docker images -q -f dangling=true)
+
+    local interactive=""
+    local force=""
+
+    while [[ $# -gt 0 ]]
+    do
+        key="$1"
+
+        case $key in
+            -h|--help)
+                echo "Usage: $0 ..."
+                ;;
+            -i|--interactive)
+                interactive=1
+                ;;
+
+            -f|--force)
+                force='-f'
+                ;;
+            *)
+                echo "Unknown option: $1"
+                exit 1
+                ;;
+        esac
+        shift
+    done
+
+
+
+    if [ -z "$interactive" ]; then
+        docker rmi $force $(docker images -q -f dangling=true)
+    else
+        local images=$(docker images \
+                           | tail -n +2 \
+                           | fzf -m \
+                                 --reverse \
+                                 --header "Select images to delete with TAB, ENTER to confirm" \
+                                 --bind "ctrl-a:select-all,ctrl-x:accept" \
+                           | awk '{ print $3 }')
+
+        for image in $(echo $images); do
+            echo docker rmi $force $image
+            docker rmi $force $image
+        done
+    fi
 }
