f5762b917bb9b997b824186538d55e40cbd175db
Roman Decker <roman.decker@gmail.com>
2019-04-27 18:28:23 +0200

    Allow `snippet` to auto-fill arguments

--- zsh-lib/snippets.sh
+++ zsh-lib/snippets.sh
@@ -4,20 +4,22 @@ function snippet {
     local s=$((cd $SNIPPET_DIR; find . -type f) | sed 's#\./##' | fzf --preview "{ highlight-snippet $SNIPPET_DIR/{} }" --height 40% --reverse)
 
     if [ ! -z "$s" ]; then
-      echo "$1"
-      if [ "$1" = "-e" ] || [ "$1" = "--eval" ]; then
-        echo "Running snippet:"
-        highlight-snippet $SNIPPET_DIR/$s
-        $SHELL $SNIPPET_DIR/$s
-      else
+        if [ "$1" = "-e" ] || [ "$1" = "--eval" ]; then
+            echo "Running snippet:"
+            highlight-snippet $SNIPPET_DIR/$s
+            $SHELL $SNIPPET_DIR/$s
 
-        if [ -t 1 ]; then
-          highlight-snippet $SNIPPET_DIR/$s
+        elif [ "$1" = "-p" ] || [ "$1" = "--print" ]; then
+            if [ -t 1 ]; then
+                highlight-snippet $SNIPPET_DIR/$s
+            else
+                cat $SNIPPET_DIR/$s
+            fi
+
         else
-          cat $SNIPPET_DIR/$s
+            local snippet=$(grep -v "^#" $SNIPPET_DIR/$s | sed "s/\\$/\\\\/g")
+            print -z "$snippet"
         fi
-
-      fi
     fi
 }
 
