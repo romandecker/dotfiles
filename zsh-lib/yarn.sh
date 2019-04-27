03b3d2189c648004353ea62ff37766ec51f2bd1e
Roman Decker <roman.decker@gmail.com>
2019-04-27 18:27:52 +0200

    Add yarn-run shortcut

---
+++ zsh-lib/yarn.sh
@@ -0,0 +1,22 @@
+findconfig() {
+  # from: https://www.npmjs.com/package/find-config#algorithm
+  # 1. If X/file.ext exists and is a regular file, return it. STOP
+  # 2. If X has a parent directory, change X to parent. GO TO 1
+  # 3. Return NULL.
+
+  if [ -f "$1" ]; then
+    printf '%s\n' "${PWD%/}/$1"
+  elif [ "$PWD" = / ]; then
+    false
+  else
+    # a subshell so that we don't affect the caller's $PWD
+    (cd .. && findconfig "$1")
+  fi
+}
+
+
+y() {
+    local packageJson=$(findconfig "package.json")
+    local script=$(jq -r '.scripts | keys []' $packageJson | fzf)
+    print -z yarn $script
+}
