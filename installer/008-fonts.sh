c61b73e305a5454a71a73aa15f15be6546b8509e
Roman Decker <roman.decker@gmail.com>
2019-11-03 16:16:32 +0100

    Merge fonts installers

--- installer/008-fonts.sh
+++ installer/008-fonts.sh
@@ -5,6 +5,18 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $SCRIPT_DIR/utils.sh
 
-pushd $SCRIPT_DIR/../submodules/powerline-fonts
+push QR$SCRIPT_DIR/../submodules/powerline-fonts
 ./install.sh
 popd
+
+TMPFILE=$(mktemp)
+
+# Download and unzip FiraCode
+curl -L https://github.com/tonsky/FiraCode/releases/latest/download/FiraCode_2.zip -o $TMPFILE
+TMPDIR=$(mktemp -d)
+unzip $TMPFILE -d $TMPDIR
+
+if [ $os == $MAC ]; then
+    # Copy fonts into macOS fonts dir
+    cp $TMPDIR/ttf/*.ttf ~/Library/Fonts/
+fi
