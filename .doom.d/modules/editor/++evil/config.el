3b7446415a2d12410be322d73c2fd91656fde7c4
Roman Decker <Roman.Decker@zooplus.com>
2020-04-09 16:20:32 +0200

    Add ++evil

---
+++ .doom.d/modules/editor/++evil/config.el
@@ -0,0 +1,14 @@
+;;; editor/++evil/config.el -*- lexical-binding: t; -*-
+
+(defmacro define-and-bind-text-object (key start-regex end-regex)
+  (let ((inner-name (make-symbol "inner-name"))
+        (outer-name (make-symbol "outer-name")))
+    `(progn
+       (evil-define-text-object ,inner-name (count &optional beg end type)
+         (evil-select-paren ,start-regex ,end-regex beg end type count nil))
+       (evil-define-text-object ,outer-name (count &optional beg end type)
+         (evil-select-paren ,start-regex ,end-regex beg end type count t))
+       (define-key evil-inner-text-objects-map ,key (quote ,inner-name))
+       (define-key evil-outer-text-objects-map ,key (quote ,outer-name)))))
+
+(define-and-bind-text-object "e" "\\`\\s-*" "\\s-*\\'")
