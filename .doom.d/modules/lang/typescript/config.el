cf80c69dbd07a941e12aa4050314fb5243efb32c
Roman Decker <Roman.Decker@zooplus.com>
2020-04-09 16:18:53 +0200

    Add typescript/eslint-disable-next-line

--- .doom.d/modules/lang/typescript/config.el
+++ .doom.d/modules/lang/typescript/config.el
@@ -76,3 +76,46 @@
                                 :includeCompletionsWithInsertText t
                                 :allowTextChangesInNewfiles t
                                 :importModuleSpecifierPreference "relative")))
+
+
+
+(defconst typescript/eslint-disable-next-line-regexp
+  "\/\/\s*eslint-disable-next-line\s+\\(.*\\)"
+  "Regexp matching an eslint flag disabling rules on the next line.")
+
+(defun typescript/add-eslint-disable-next-line ()
+  "Add an eslint flag to disable rules generating errors at point.
+
+This function adds or modifies a flag of this form to the
+previous line:
+
+  // eslint-disable-next-line [rule1] [rule2] [...]
+
+The line will be indented according to the current indentation
+settings.  This function generates rule1, rule2 to cover all the
+errors present at point.
+
+If the previous line does not already contain a disable-next-line
+flag, a new line is added to hold the new flag.  If the previous
+line already contains a disable-next-line flag, the rule is added
+to the flag.  Note that this function does not preserve the
+formatting of the already existing flag.  The resulting flag will
+always be formatted as described above."
+  (interactive)
+  (let ((error-ids (delq nil (tide-get-flycheck-errors-ids-at-point)))
+        (start (point)))
+    (when error-ids
+      (save-excursion
+        (if (and (eq 0 (forward-line -1))
+                 (looking-at tide-tslint-disable-next-line-regexp))
+            ;; We'll update the old flag.
+            (let ((old-list (split-string (match-string 1))))
+              (delete-region (point) (point-at-eol))
+              (setq error-ids (append old-list error-ids)))
+          ;; We'll create a new flag.
+          (goto-char start)
+          (beginning-of-line)
+          (open-line 1))
+        (insert "// eslint-disable-next-line "
+                (string-join error-ids " "))
+        (typescript-indent-line)))))
