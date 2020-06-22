360e8f71b6696fad1d569895d979d7016605d533
Roman Decker <Roman.Decker@zooplus.com>
2020-06-22 14:27:29 +0200

    Fix evil-move

--- .doom.d/modules/editor/evil-move/config.el
+++ .doom.d/modules/editor/evil-move/config.el
@@ -2,22 +2,14 @@
 ;;; This module makes all deletions via "d" go to black-hole
 ;;; Instead, it maps the "m" key ("move") to cut
 
-(defvar +my/is-move nil
-  "Temporary variable to tell if the current evil-delete command is actually a move command")
-
-(defun +my/evil-delete-advice
-    (orig-fn beg end &optional type register &rest args)
-  "Advice to add around evil-delete that will check `+my/is-move' and only if it
-is set to t will it write to the passed register. If not, the deletion will go
-to the blackhole register."
-  (apply orig-fn beg end type (if +my/is-move register ?_) args))
-
 (after! evil
-  (advice-add 'evil-delete :around '+my/evil-delete-advice))
+  (evil-define-operator evil-destroy (beg end type)
+    "Delete text from BEG to END with TYPE. Just like evil-delete but always
+deletes to black-hole"
+    (evil-delete beg end type ?_ nil))
 
-(defun +my/evil-move ()
-  (interactive)
-  (let ((+my/is-move t))
-    (call-interactively #'evil-delete)))
+  (map! :n "d" #'evil-destroy)
+  (map! :n "m" #'evil-delete))
 
-(map! :n "m" #'+my/evil-move)
+(after! evil-surround
+  (add-to-list 'evil-surround-operator-alist '(evil-destroy . delete)))
