8a50c285b914f51d5c7d43d5cc7df3b0862efef8
Roman Decker <roman.decker@gmail.com>
2022-10-23 10:46:40 +0200

    Fix pet installer

--- installer/009-node.sh
+++ installer/009-node.sh
@@ -8,7 +8,7 @@ source $SCRIPT_DIR/utils.sh
 # Note that nvm is now managed via the lukechilds/zsh-nvm zsh-plugin
 
 if [ -x "$(command -v yvm)" ]; then
-    cecho "$fawn$command$normal is already installed"
+    cecho "$fawn$" yvm "$normal is already installed"
 else
     curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.sh | bash
 fi
