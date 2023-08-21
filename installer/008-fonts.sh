56262d2ba675f03da92cad850f1e83a51d9e6ede
Roman Decker <roman.decker@gmail.com>
2023-08-21 10:54:35 +0200

    Refactor fonts installer

--- installer/008-fonts.sh
+++ installer/008-fonts.sh
@@ -5,11 +5,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $SCRIPT_DIR/utils.sh
 
-# pushd $SCRIPT_DIR/../submodules/powerline-fonts
-# ./install.sh
-# popd
-
-
+pushd $SCRIPT_DIR/../submodules/powerline-fonts
+./install.sh
+popd
 
 
 FONT_ZIP_URLS=(
@@ -17,15 +15,15 @@ FONT_ZIP_URLS=(
     "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Agave.zip"
 )
 
-for url in "${FONT_ZIP_URLS[@]}"; do
-    echo "Installing fonts from $url..."
-    TMPFILE=$(mktemp)
-    curl -L $url -o $TMPFILE
-    TMPDIR=$(mktemp -d)
-    unzip $TMPFILE -d $TMPDIR
-    if [ $os == $MAC ]; then
-        # Copy fonts into macOS fonts dir
+if [ $os == $MAC ]; then
+    # Copy fonts into macOS fonts dir
+    for url in "${FONT_ZIP_URLS[@]}"; do
+        echo "Installing fonts from $url..."
+        TMPFILE=$(mktemp)
+        curl -L $url -o $TMPFILE
+        TMPDIR=$(mktemp -d)
+        unzip $TMPFILE -d $TMPDIR
         cp $TMPDIR/**/*.ttf ~/Library/Fonts/ || true
         cp $TMPDIR/*.ttf ~/Library/Fonts/ || true
-    fi
-done <<< $FONT_ZIP_URLS
+    done <<< $FONT_ZIP_URLS
+fi
