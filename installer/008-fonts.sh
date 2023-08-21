b85cfdb45b7842bc287ae12ae07ab9e0e1393acd
Roman Decker <roman.decker@gmail.com>
2023-08-21 10:52:58 +0200

    Add tmux catppuccin theme

--- installer/008-fonts.sh
+++ installer/008-fonts.sh
@@ -5,18 +5,27 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $SCRIPT_DIR/utils.sh
 
-pushd $SCRIPT_DIR/../submodules/powerline-fonts
-./install.sh
-popd
+# pushd $SCRIPT_DIR/../submodules/powerline-fonts
+# ./install.sh
+# popd
 
-TMPFILE=$(mktemp)
 
-# Download and unzip FiraCode
-curl -L https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip -o $TMPFILE
-TMPDIR=$(mktemp -d)
-unzip $TMPFILE -d $TMPDIR
 
-if [ $os == $MAC ]; then
-    # Copy fonts into macOS fonts dir
-    cp $TMPDIR/ttf/*.ttf ~/Library/Fonts/
-fi
+
+FONT_ZIP_URLS=(
+    "https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip"
+    "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Agave.zip"
+)
+
+for url in "${FONT_ZIP_URLS[@]}"; do
+    echo "Installing fonts from $url..."
+    TMPFILE=$(mktemp)
+    curl -L $url -o $TMPFILE
+    TMPDIR=$(mktemp -d)
+    unzip $TMPFILE -d $TMPDIR
+    if [ $os == $MAC ]; then
+        # Copy fonts into macOS fonts dir
+        cp $TMPDIR/**/*.ttf ~/Library/Fonts/ || true
+        cp $TMPDIR/*.ttf ~/Library/Fonts/ || true
+    fi
+done <<< $FONT_ZIP_URLS
